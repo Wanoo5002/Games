a432fe2a571017351f51c66cc492ff7838952dc4
Wanoo5002 <iandreev329@gmail.com>
2024-11-05 19:50:13 +0300

    avg pool

--- src/components/preferans/App.tsx
+++ src/components/preferans/App.tsx
@@ -6,6 +6,8 @@ type PlayerResult = {
     whist: number[]
 }
 
+
+
 let state: PlayerResult[][] = [
     [],
     [],
@@ -32,7 +34,18 @@ function formatHistory(history: number[]) {
     if (result.length === 0) return ''
     let n = history.at(-1) === 0 ? 1 : 2
     return `.${result.slice(-n).join('.')}`
-    
+}
+
+function result() {
+    let sumPool = 0
+    for (let i = 0; i < state.length; i++) {
+        let playerSum = 0
+        for (let j = 0; j < state[i].length; j++) {
+            playerSum += state[i][j].pool
+        }
+        sumPool += playerSum
+    }
+    return sumPool / 3
 }
 
 addScore([
@@ -107,19 +120,24 @@ addScore([
     }
 ])
 
-export function App() {
-    const [count, setCount] = useState(0)
-    
+function whist(i: number, w: number) {
+    // return (i + w + 1) % 3 + 1
+    let players: number[] = [1, 2, 3]
+    players.splice(i, 1)
+    return players[w]
+} 
+
+export function App() {  
     return (
         <div>
-            <div>{count}</div>
-            <button onClick={() => {console.log('count'); setCount(count + 1)}}>count++</button>
-
+            <div>Pool result: {result()}</div>
             {state.map((s, i) =>
                 <div style={{marginBottom: 30}}>    
                     <div>Player {i + 1}</div>
                     <div>m: {formatHistory(s.map(r => r.mountain))}</div>
                     <div>p: {formatHistory(s.map(r => r.pool))}</div>
+                    <div>w{whist(i, 0)}: {formatHistory(s.map(r => r.whist[0]))}</div>
+                    <div>w{whist(i, 1)}: {formatHistory(s.map(r => r.whist[1]))}</div>
                 </div>
             )}
         </div>
